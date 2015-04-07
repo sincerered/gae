574acc5750f845f73c4922ca7537edb0e114fc52
sincerered <sincerered@gmail.com>
2015-04-07 19:15:17 +0800

    haha1

--- getminutedata.py
+++ getminutedata.py
@@ -17,15 +17,13 @@ from google.appengine.api import app_identity
 class MainPage(webapp2.RequestHandler):
   """Main page for GCS demo application."""
 
-  def get(self):
+	def get(self):
     
+		try:
+			url = urllib2.urlopen("http://info512.taifex.com.tw/Future/FusaQuote_Norl.aspx?d=3000300").read().decode("utf-8")
 
-	
-	try:
-		url = urllib2.urlopen("http://info512.taifex.com.tw/Future/FusaQuote_Norl.aspx?d=3000300").read().decode("utf-8")
-
-		x = etree.HTML(url).xpath(
-			'//span[@id="ctl00_ContentPlaceHolder1_uc_DgFusaQuote1_dgData_ctl03_lblItemStatus"]/parent::font/parent::td/following-sibling::td/font')
+			x = etree.HTML(url).xpath(
+				'//span[@id="ctl00_ContentPlaceHolder1_uc_DgFusaQuote1_dgData_ctl03_lblItemStatus"]/parent::font/parent::td/following-sibling::td/font')
 		except:
 			self.response.write("error")
 		else:	
@@ -34,7 +32,7 @@ class MainPage(webapp2.RequestHandler):
 			
 			
 
-  
+#kkkk  
 
 app = webapp2.WSGIApplication([('/getminutedata', MainPage)],
                               debug=True)
