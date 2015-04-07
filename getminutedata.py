23ff76bd5b16133820bbc9d986e9b0f4658d0e33
sincerered <sincerered@gmail.com>
2015-04-07 19:04:06 +0800

    haha

---
+++ getminutedata.py
@@ -0,0 +1,41 @@
+# Copyright 2012 Google Inc. All Rights Reserved.
+
+from lxml import etree
+import datetime
+import logging
+import os
+import cloudstorage as gcs
+import webapp2
+import sys
+import urllib2
+
+from google.appengine.api import app_identity
+
+
+
+
+class MainPage(webapp2.RequestHandler):
+  """Main page for GCS demo application."""
+
+  def get(self):
+    
+
+	
+	try:
+		url = urllib2.urlopen("http://info512.taifex.com.tw/Future/FusaQuote_Norl.aspx?d=3000300").read().decode("utf-8")
+
+		x = etree.HTML(url).xpath(
+			'//span[@id="ctl00_ContentPlaceHolder1_uc_DgFusaQuote1_dgData_ctl03_lblItemStatus"]/parent::font/parent::td/following-sibling::td/font')
+		except:
+			self.response.write("error")
+		else:	
+			for i in x:
+				self.response.write("{}\t".format(i.text))
+			
+			
+
+  
+
+app = webapp2.WSGIApplication([('/getminutedata', MainPage)],
+                              debug=True)
+
