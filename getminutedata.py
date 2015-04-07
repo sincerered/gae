27c6ef95bd0a0b3648fc2895fed385a3e0bf70d6
sincerered <sincerered@gmail.com>
2015-04-07 19:18:31 +0800

    dsaf

--- getminutedata.py
+++ getminutedata.py
@@ -19,7 +19,7 @@ class MainPage(webapp2.RequestHandler):
 
 	def get(self):
     
-		try:
+"""		try:
 			url = urllib2.urlopen("http://info512.taifex.com.tw/Future/FusaQuote_Norl.aspx?d=3000300").read().decode("utf-8")
 
 			x = etree.HTML(url).xpath(
@@ -29,8 +29,8 @@ class MainPage(webapp2.RequestHandler):
 		else:	
 			for i in x:
 				self.response.write("{}\t".format(i.text))
-			
-			
+"""			
+		self.response.write("kerker")		
 
 #kkkk  
 
